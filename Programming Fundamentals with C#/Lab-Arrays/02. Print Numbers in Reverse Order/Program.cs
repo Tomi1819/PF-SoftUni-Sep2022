849f0621bbd0c06daf487a2f564d8936a90b5dd3
Tomi1819 <toshevtomi@gmail.com>
2022-10-03 22:18:38 +0300

    arrays-lab

---
+++ Programming Fundamentals with C#/Lab-Arrays/02. Print Numbers in Reverse Order/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02._Print_Numbers_in_Reverse_Order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int[] numbers = new int[num];
+
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                int currNum = int.Parse(Console.ReadLine());
+                numbers[i] = currNum;
+            }
+            Console.WriteLine(string.Join(' ', numbers)) ;
+
+        }
+    }
+}
---
+++ Programming Fundamentals with C#/Lab-Arrays/03. Rounding Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03._Rounding_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
