c15bc374fd544d43f567f7dfa311fa924140bd5e
Tomi1819 <toshevtomi@gmail.com>
2022-10-08 12:54:53 +0300

    Exercise-Arrays

---
+++ Programming Fundamentals with C#/Arrays-Exercise/01. Train/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01._Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfWagons = int.Parse(Console.ReadLine());
+            int[] peopleInWagon = new int[countOfWagons];
+            int sum = 0;
+            for (int i = 0; i < countOfWagons; i++)
+            {
+                peopleInWagon[i] = int.Parse(Console.ReadLine());
+                Console.Write($"{peopleInWagon[i]} ");
+                sum += peopleInWagon[i];
+            }
+            Console.WriteLine();
+            Console.WriteLine(sum);
+
+        }
+    }
+}
