6d5df7a2145fb8e3b50ad8f58e67cb1f7d29046c
Tomi1819 <toshevtomi@gmail.com>
2022-10-08 23:33:33 +0300

    :+1:

---
+++ Programming Fundamentals with C#/Arrays-Exercise/05. Top Integers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Top_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currNum = numbers[i];
+                bool isTopInteger = true;
+                for (int k = i+1; k < numbers.Length; k++)
+                {
+                    int nextNum = numbers[k];
+                    if (nextNum>=currNum)
+                    {
+                        isTopInteger = false;
+                        break;
+                    }
+                }
+                if (isTopInteger)
+                {
+                    Console.Write(currNum + " ");
+                }
+            }
+        }
+    }
+}
--- Programming Fundamentals with C#/Arrays-Exercise/3. Zig-Zag Arrays/Program.cs
+++ Programming Fundamentals with C#/Arrays-Exercise/3. Zig-Zag Arrays/Program.cs
@@ -1,12 +1,37 @@
-﻿using System;
-
-namespace _3._Zig_Zag_Arrays
+﻿namespace _3._Zig_Zag_Arrays
 {
+    using System;
+    using System.Linq;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr1 = new int[n];
+            int[] arr2 = new int[n];
+            for (int i = 1; i <= n; i++)
+            {
+                int[] numbers = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+                int firstNum = numbers[0];
+                int secondNum = numbers[1];
+
+                if (i % 2 != 0)
+                {
+                    arr1[i - 1] = firstNum;
+                    arr2[i - 1] = secondNum;
+                }
+                else
+                {
+                    arr1[i - 1] = secondNum;
+                    arr2[i - 1] = firstNum;
+                }
+            }
+            Console.WriteLine(string.Join(" ", arr1));
+            Console.WriteLine(string.Join(" ", arr2));
         }
     }
 }
