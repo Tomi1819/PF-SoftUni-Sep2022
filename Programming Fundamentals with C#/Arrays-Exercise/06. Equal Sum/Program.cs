8ae996682444148b304c29ec16741514d5a95b04
Tomi1819 <toshevtomi@gmail.com>
2022-10-09 13:07:12 +0300

    6,7,8

--- Programming Fundamentals with C#/Arrays-Exercise/06. Equal Sum/Program.cs
+++ Programming Fundamentals with C#/Arrays-Exercise/06. Equal Sum/Program.cs
@@ -1,12 +1,37 @@
-﻿using System;
-
-namespace _06._Equal_Sum
+﻿namespace _06._Equal_Sum
 {
+    using System;
+    using System.Linq;
     class Program
     {
         static void Main(string[] args)
         {
-            
+            int[] num = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            if (num.Length == 1)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+            int leftSum = 0;
+            int rightSum = 0;
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                leftSum = num.Take(i).Sum();
+
+                rightSum = num.Skip(i + 1).Sum();
+
+                if (leftSum == rightSum)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+            Console.WriteLine("no");
         }
     }
 }
